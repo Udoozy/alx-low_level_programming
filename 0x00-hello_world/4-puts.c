c44e72a9aa452bbe35214e377cc600cb8e3d80dc
Udoozy <agbo4samson4gmail.com>
2023-12-14 13:45:56 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints string with puts to std output
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
