6c1bfbd4534c2702fe421a808e02551514703ef5
Udoozy <agbo4samson4gmail.com>
2023-12-20 15:02:33 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints jack br every minutes
+ *
+ * Return: Always success
+ */
+void jack_bauer(void)
+{
+	int l, m;
+
+	for (l = 0; l < 24; l++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(l / 10 + '0');
+			_putchar(1 % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
