e38f20524a65838d5fa9a07dc2d9b849773a747b
Udoozy <agbo4samson4gmail.com>
2023-12-20 13:20:55 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print 10x alphabets
+ *
+ * Return: Always return success
+ */
+void print_alphabet_x10(void);
+{
+	char a = 'a';
+	int b = 0;
+
+	while (b < 10)
+	{
+		while (a <= 'a')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
