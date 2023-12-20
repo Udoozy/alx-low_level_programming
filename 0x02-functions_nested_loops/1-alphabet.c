4c16d80b5277a618ed3a9a0d4e0c48ac5ae0a72d
Udoozy <agbo4samson4gmail.com>
2023-12-20 12:58:44 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Print alphabets in lower case
+ *
+ * Return: ALways r
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}
