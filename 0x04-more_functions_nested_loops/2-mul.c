aa287217d3f9d4b882ef3de4d9f4fce2b30782ff
Udoozy <agbo4samson4gmail.com>
2023-12-25 13:19:26 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * mul - Multiply two intergers
- *
- * return: Always return result
+ * @x: First parameter
+ * @y: Second Parameter
+ * Return: Always return result
  */
 int mul(int x, int y)
 {
 	int result = (x * y);
-	return result;
+
+	return (result);
 }
