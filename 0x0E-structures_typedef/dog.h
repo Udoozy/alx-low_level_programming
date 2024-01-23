ba3d31f9ea67c5abd1152273421548cde29972ea
Udoozy <agbo4samson@gmail.com>
2024-01-23 15:46:40 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct name
+ * @name: Dogs name
+ * @age: Dogs aage
+ * @owner: Dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
