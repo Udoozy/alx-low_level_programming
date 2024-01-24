a2bdf66a75520c94e9988b4baa120851682e93bc
Udoozy <agbo4samson@gmail.com>
2024-01-24 15:56:31 -0800

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
-#ifndef HEADER_FILE_H
-#define HEADER_FILE_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* HEADER_FILE_H */
+#endif
