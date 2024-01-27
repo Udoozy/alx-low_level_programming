bed6c39de5637fe7ac56f03f787744cb79815eac
Udoozy <agbo4samson@gmail.com>
2024-01-27 14:26:58 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,8 @@
  * @argv: Argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	(void) argc;
-
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
-
 }
