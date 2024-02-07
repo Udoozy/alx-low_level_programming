4918c130ced1a66cfc182f563e47d1b00e60e8c7
Udoozy <agbo4samson@gmail.com>
2024-02-07 13:07:55 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Prints number of element
+ * @h: An argument for head pointer
+ * Return: Always number of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter;
+
+	if (h == 0)
+		return (0);
+	for (counter = 0; n != NULL; counter++)
+	{
+		h = h->next;
+	}
+	return (counter);
+}
