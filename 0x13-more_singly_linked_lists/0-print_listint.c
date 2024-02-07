ba8a4eef8fa1fef1cb8fc9443ad2c2d7e18b6122
Udoozy <agbo4samson@gmail.com>
2024-02-07 12:18:08 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - Print all element of listint
+ * @h: An Argument for the head pointer
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int counter;
+
+	if (h == NULL)
+		return (0);
+	for (counter = 0; h != NULL; counter++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (counter);
+
+
+}
