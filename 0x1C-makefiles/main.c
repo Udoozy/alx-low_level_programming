f410664c30473009d2be2997f908ce0476a5823d
Udoozy <agbo4samson@gmail.com>
2024-05-29 15:05:26 -0700

    0-Makefile

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,11 @@
+#include "mak.h"
+/**
+ * main - Print entry level
+ *
+ * Return: Always Exit success
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/mak.h
@@ -0,0 +1,9 @@
+#ifndef __MAK__
+#define __MAK__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_school(void);
+
+#endif
