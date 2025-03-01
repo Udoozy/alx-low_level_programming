c69efe3648ff1476159748967e75775b1cafed8f
Udoozy <agbo4samson@gmail.com>
2025-03-01 17:25:30 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - The function used for capitalizing
+ * @str: The string to capitalize
+ * Return: Always return a pointer
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	char separator[] = "\t\n,;.!?\"(){}";
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
+
+	while (str[i] != '\0')
+	{
+		int j;
+
+		for (j = 0; separator[j] != '\0'; j++)
+		{
+			if (str[i] == separator[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+
+}
