b6c4efe0169dd04a63a4b7535f78894a83dd3bf2
Udoozy <agbo4samson@gmail.com>
2025-03-13 12:39:41 +0100

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - The function defined
+ * @s: the pointer to the pointer to an array
+ * @to: the pinter to the array changed
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
