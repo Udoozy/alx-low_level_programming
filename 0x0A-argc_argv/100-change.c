59148ac8727c8654c841a8e6e6bb7c9ad4dae982
Udoozy <agbo4samson@gmail.com>
2025-03-23 19:36:40 +0100

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The main function
+ * @argc: The Argument counter
+ * @argv: The argument vector
+ * Return: Always an integer
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int cent, coins = 0;
+	int denomination[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cent = atoi(argv[1]);
+
+	if (cent < 0)
+	{
+		printf("0\n");
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		while (cent >= denomination[i])
+		{
+			cent -= denomination[i];
+			coins++;
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
