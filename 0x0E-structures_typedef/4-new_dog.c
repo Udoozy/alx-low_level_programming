adad0843643b2e225394dde6f8dfb35c4681ee33
Udoozy <agbo4samson@gmail.com>
2025-04-13 22:13:38 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int name_len = 0, owner_len = 0;
+	int name_len = 0, owner_len = 0, i;
 
 	while (name[name_len])
 		name_len++;
@@ -35,11 +35,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (int i = 0; i < name_len; i++)
+	for (i = 0; i < name_len; i++)
 		dog->name[i] = name[i];
 	dog->name[name_len] = '\0';
 
-	for (int i = 0; i < owner_len; i++)
+	for (i = 0; i < owner_len; i++)
 		dog->owner[i] = owner[i];
 	dog->owner[owner_len] = '\0';
 
